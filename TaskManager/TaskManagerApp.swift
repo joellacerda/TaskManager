4310a6ae7465ebe46dadd54093b7ee5700401fc5
Joel Lacerda <joellacerdaol@gmail.com>
2023-09-22 13:51:05 -0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Joel Lacerda on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
